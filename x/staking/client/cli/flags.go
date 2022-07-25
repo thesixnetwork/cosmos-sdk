666f73e36733c1ec2a28b50bf98cf709f9fb3376
dDee-D <hamdee.dev001@gmail.com>
2022-07-25 18:05:40 +0700

    merge with cosmos v0.45.6

--- x/staking/client/cli/flags.go
+++ x/staking/client/cli/flags.go
@@ -20,6 +20,7 @@ const (
 	FlagSharesFraction      = "shares-fraction"
 
 	FlagMoniker         = "moniker"
+	FlagEditMoniker     = "new-moniker"
 	FlagIdentity        = "identity"
 	FlagWebsite         = "website"
 	FlagSecurityContact = "security-contact"
@@ -149,7 +150,7 @@ func FlagSetPublicKey() *flag.FlagSet {
 func flagSetDescriptionEdit() *flag.FlagSet {
 	fs := flag.NewFlagSet("", flag.ContinueOnError)
 
-	fs.String(FlagMoniker, types.DoNotModifyDesc, "The validator's name")
+	fs.String(FlagEditMoniker, types.DoNotModifyDesc, "The validator's name")
 	fs.String(FlagIdentity, types.DoNotModifyDesc, "The (optional) identity signature (ex. UPort or Keybase)")
 	fs.String(FlagWebsite, types.DoNotModifyDesc, "The validator's (optional) website")
 	fs.String(FlagSecurityContact, types.DoNotModifyDesc, "The validator's (optional) security contact email")
