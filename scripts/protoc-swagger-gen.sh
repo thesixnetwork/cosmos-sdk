d2c722f34cc20725f321d2625b842ecc538ea9f0
dDeeDev <dee.hamdee.084@gmail.com>
2024-09-11 17:12:33 +0700

    :bug: fix path query

--- scripts/protoc-swagger-gen.sh
+++ scripts/protoc-swagger-gen.sh
@@ -9,7 +9,7 @@ for dir in $proto_dirs; do
   # generate swagger files (filter query files)
   query_file=$(find "${dir}" -maxdepth 1 \( -name 'query.proto' -o -name 'service.proto' \))
   if [[ ! -z "$query_file" ]]; then
-    buf protoc  \
+     protoc  \
       -I "proto" \
       -I "third_party/proto" \
       "$query_file" \
@@ -21,7 +21,7 @@ done
 # combine swagger files
 # uses nodejs package `swagger-combine`.
 # all the individual swagger files need to be configured in `config.json` for merging
-swagger-combine ./client/docs/config.json -o ./client/docs/swagger-ui/swagger.yaml -f yaml --continueOnConflictingPaths true --includeDefinitions true
+npx swagger-combine ./client/docs/config.json -o ./client/docs/swagger-ui/swagger.yaml -f yaml --continueOnConflictingPaths true --includeDefinitions true
 
 # clean swagger files
 rm -rf ./tmp-swagger-gen
