09b99b03011b8daf6999a810262f88b55732ac79
dDeeDev <dee.hamdee.084@gmail.com>
2024-10-22 15:29:04 +0700

    feat(staking): :sparkles: validator_approval

--- x/staking/autocli.go
+++ x/staking/autocli.go
@@ -21,6 +21,11 @@ func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 					Long:      "Query details about all validators on a network.",
 				},
 				{
+					RpcMethod: "ValidatorApproval",
+					Short:     "Query a validator approval",
+					Long:      "Query details about validator approval.",
+				},
+				{
 					RpcMethod: "Validator",
 					Use:       "validator [validator-addr]",
 					Short:     "Query a validator",
